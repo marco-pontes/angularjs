c0e31848cc0648f9cc053ac6a4e3cac0b8fb8c98
mpontes <marco.pontes@neogrid.com>
2017-09-26 10:14:37 -0300

    Removing unecessary structures and adding gitignore

--- rd-tracker/target/rdtracker-0.1-SNAPSHOT 3/WEB-INF/classes/js/app.config.js
+++
@@ -1,8 +0,0 @@
-app.config(function($routeProvider){
-	$routeProvider.
-	when("/", {templateUrl:'resources/html/default.html', controller:'MainController'}).
-	when("/user/create", {templateUrl:'resources/html/user/create.html', controller:'UserController'}).
-	otherwise({redirectTo:'/'});
-});
-
-
--- rd-tracker/target/rdtracker-0.1-SNAPSHOT/js/app/app.config.js
+++
@@ -1,8 +0,0 @@
-app.config(function($routeProvider){
-	$routeProvider.
-	when("/", {templateUrl:'html/default.html', controller:'MainController'}).
-	when("/user/index", {templateUrl:'html/user/index.html', controller:'UserController'}).
-	otherwise({redirectTo:'/'});
-});
-
-
